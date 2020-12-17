34f5efad474fd1685f04eaaeb7394d2a569810dc
James Christopher Wingeier II <52218669+jcwii@users.noreply.github.com>
2020-12-17 08:16:08 -0500

    Add files via upload

---
+++ print-html-img-src-and-alt-attributes.py
@@ -0,0 +1,16 @@
+import os
+
+folder = '' #path to folder containing .jpg and .png files
+
+def imgSrcs(folder):
+    for file in os.listdir(folder):
+        if file.endswith('.jpg' or '.png'):
+            print(file)
+
+def imgAlts(folder):
+    files = [os.path.splitext(filename)[0] for filename in os.listdir(folder) if filename.endswith('.jpg' or '.png')]
+    for file in files:
+        print (file.replace('-', ' '))
+
+#call imgSrcs(folder) from terminal
+#call imgAlts(folder) from terminal
