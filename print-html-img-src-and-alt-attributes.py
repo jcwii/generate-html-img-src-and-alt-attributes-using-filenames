78fe1465a40f1374cf2ef6f65801e5a70a0819e0
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-01-07 17:40:58 -0500

    Update print-html-img-src-and-alt-attributes.py

--- print-html-img-src-and-alt-attributes.py
+++ print-html-img-src-and-alt-attributes.py
@@ -1,16 +1,7 @@
 import os
-
-folder = '' #path to folder containing .jpg and .png files
-
-def imgSrcs(folder):
-    for file in os.listdir(folder):
-        if file.endswith('.jpg' or '.png'):
-            print(file)
-
-def imgAlts(folder):
-    files = [os.path.splitext(filename)[0] for filename in os.listdir(folder) if filename.endswith('.jpg' or '.png')]
-    for file in files:
-        print (file.replace('-', ' ')) #replacement for hyphen-separated filenames
-
-#call imgSrcs(folder) from terminal
-#call imgAlts(folder) from terminal
+#dir=''
+for p,ds,fs in os.walk(dir):
+    print('#### directory of following srcs/alts is '+p)
+    for f in fs:
+        if f.endswith(('jpg','jpeg' or 'png')):
+            print('src="'+f+'" '+'alt="'+os.path.splitext(f)[0].replace('-',' ')+'"')
